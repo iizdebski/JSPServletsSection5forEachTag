d470ed49ae9aeb88e6dcf18867a8c7ad531f2421
ivan <iizdepskyy112@gmail.com>
2019-09-13 14:50:15 +0300

    15. Servlet Life Cycle Methods

--- src/main/java/com/izdebski/FirstServlet.java
+++ src/main/java/com/izdebski/FirstServlet.java
@@ -10,6 +10,11 @@ import java.io.PrintWriter;
 
 public class FirstServlet extends HttpServlet {
 
+    public FirstServlet(){
+        super();
+        System.out.println("Inside the no-arg constructor():::::");
+    }
+
     //doPost
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
@@ -20,5 +25,23 @@ public class FirstServlet extends HttpServlet {
         req.setAttribute("name", fullName);
         RequestDispatcher dispatcher = req.getRequestDispatcher("/output.jsp");
         dispatcher.forward(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        System.out.println("Inside the init():::::");
+        System.out.println("Inside the init():::::");
+    }
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+        System.out.println("Inside the service():::::");
+    }
+    @Override
+    public void destroy() {
+
+        super.destroy();
+        System.out.println("Inside the destroy():::::");
     }
 }
